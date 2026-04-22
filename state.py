bbbbce6311af1018ad95d02aa6ade5c584e58b45
SOURAV SUBHAM <souravsubham@Mac.bbrouter>
2026-04-22 13:42:24 +0530

    Added conversation state management for lead capture flow

---
+++ state.py
@@ -0,0 +1,7 @@
+class AgentState:
+    def __init__(self):
+        self.intent = None
+        self.name = None
+        self.email = None
+        self.platform = None
+        self.stage = "start"
